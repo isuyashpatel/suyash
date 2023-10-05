97291fc0c419559c449d7a19540b304eca12a568
isuyashpatel <suyashpatelmrj@gmail.com>
2023-10-06 03:53:39 +0530

    framer motion

--- src/app/component/social/index.js
+++ src/app/component/social/index.js
@@ -1,23 +1,47 @@
 import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from "react-icons/ai"
 import { SiLeetcode } from "react-icons/si"
-
+import { motion } from "framer-motion"
 const Social = () => {
     return (
         <div className='lg:text-5xl text-2xl flex justify-center lg:gap-16 gap-10 text-gray-600 dark:text-gray-400'>
-            <a href='https://github.com/isuyashpatel' target={'_blank'}>
-                <AiFillGithub />
-            </a>
-            <a href='https://leetcode.com/serpensortia/' target={'_blank'}>
-                <SiLeetcode />
-            </a>
-            <a
-                href='https://www.linkedin.com/in/suyash-patel-0b6199191/'
-                target={'_blank'}>
-                <AiFillLinkedin />
-            </a>{' '}
-            <a href='https://twitter.com/isuyashpatel' target={'_blank'}>
-                <AiFillTwitterCircle />
-            </a>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, x: 100 }}
+                transition={{ delay: 0.8 }}
+            >
+                <a href='https://github.com/isuyashpatel' target={'_blank'}>
+                    <AiFillGithub />
+                </a>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, x: 100 }}
+                transition={{ delay: 1 }}
+            >
+                <a href='https://leetcode.com/serpensortia/' target={'_blank'}>
+                    <SiLeetcode />
+                </a>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, x: 100 }}
+                transition={{ delay: 1.2 }}
+            >
+                <a
+                    href='https://www.linkedin.com/in/suyash-patel-0b6199191/'
+                    target={'_blank'}>
+                    <AiFillLinkedin />
+                </a>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, x: 100 }}
+                transition={{ delay: 1.4 }}
+            >
+                <a href='https://twitter.com/isuyashpatel' target={'_blank'}>
+                    <AiFillTwitterCircle />
+                </a>
+            </motion.div>
         </div>
     )
 }
