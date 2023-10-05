f66536d8b777455593fbfc641cdee7baa3834cac
isuyashpatel <suyashpatelmrj@gmail.com>
2023-10-05 19:59:03 +0530

    Typewriter

--- src/app/component/about/index.js
+++ src/app/component/about/index.js
@@ -10,23 +10,25 @@ const About = () => {
             <div className='mx-auto bg-gradient-to-b from-teal-500 rounded-full lg:w-96 lg:h-96 h-48 w-48 relative mt-6 overflow-hidden'>
                 <Image src={suyash} fill className='object-cover' alt='Profile' />
             </div>
-            <div className='flex flex-col space-y-6 text-center mt-10'>
+            <div className='flex flex-col gap-y-6 text-center mt-10'>
                 <h2 className='lg:text-5xl text-2xl dark:text-white text-teal-600 font-medium'>
                     Suyash Patel
                 </h2>
-                <h3 className='lg:text-2xl h-10 text-gray-800 dark:text-gray-200 text-lg  w-screen overflow-y-hidden'>
+                <div>
                     <Typewriter
                         options={{
                             strings: [
-                                '<h3>Full Stack Developers</h3>',
+                                '<h3>Full Stack Developer</h3>',
                                 '<h3>Quick Learner</h3>',
                             ],
                             autoStart: true,
                             loop: true,
+                            cursorClassName: 'hidden',
+                            wrapperClassName: 'lg:text-2xl text-gray-800 dark:text-gray-200 text-lg w-screen overflow-y-hidden'
                         }}
                     />
-                </h3>
-                <h4 className='semi-bold lg:text-2xl text-lg px-4 md:px-8 leading-8 text-gray-800 dark:text-gray-200'>
+                </div>
+                <h4 className='semi-bold lg:text-2xl text-lg px-4 md:px-8 leading-8 text-gray-800 dark:text-gray-200 mt-6'>
                     I am Software Developer with strong passion and dedication for self - improvement. I love to
                     learn new technolgies and to create new stuffs related to them.
                 </h4>
